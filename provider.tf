dbcb4ea8b8f364553f60f1cd587d3decdc58832c
TheCloudCTO <jhowerin@cloudsm.art>
2020-07-17 21:25:07 -0400

    Creating EC2 with GitHub actions

--- provider.tf
+++ provider.tf
@@ -4,13 +4,13 @@ provider "aws" {
 }
 
 
-#resource "aws_instance" "temporary-instance" {
-#  ami           = "ami-2757f631"
-#  instance_type = "t2.micro"
-#  tags = {
-#    Name = var.the_name
-#  }
-#}
+resource "aws_instance" "temporary-instance" {
+ ami           = "ami-2757f631"
+ instance_type = "t2.micro"
+ tags = {
+   Name = var.the_name
+ }
+}
 
 terraform {
   backend "s3" {
